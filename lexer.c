db168d7a1fce01b93e06f74e569bef714d3a9c9c
4lexK10 <128997122+4lexK10@users.noreply.github.com>
2024-07-11 00:24:07 +0200

    Add files via upload

--- lexer.c
+++ lexer.c
@@ -3,21 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   lexer.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: akloster <akloster@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/09 17:26:54 by akloster          #+#    #+#             */
-/*   Updated: 2024/07/09 20:11:35 by akloster         ###   ########.fr       */
+/*   Updated: 2024/07/11 00:23:39 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static bool	check_word(char *str)
+static bool	check_token(char *str)
 {
 	if (*str == '<' || *str == '>' || *str == '|' || ft_strnstr(str, "<<", 2)
 			|| ft_strnstr(str, ">>", 2))
-		return (false);
-	return (true);
+		return (true);
+	return (false);
 }
 
 t_data	*lexer(char *arg)
@@ -29,9 +29,9 @@ t_data	*lexer(char *arg)
 	i = -1;
 	while (arg[i])
 	{
-		if (check_word(args[i]))
+		if (check_token(args[i]))
 		{
-			
+				
 		}	
 	}
 	return (head);
