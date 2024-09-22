60137479f451edb3f772e74a14b44718d8af88c9
akloster <akloster@student.s19.be>
2024-09-22 18:37:53 +0200

    fix(cd): cd env update works

--- builtins/ft_cd.c
+++ builtins/ft_cd.c
@@ -6,33 +6,60 @@
 /*   By: akloster <akloster@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 23:23:57 by akloster          #+#    #+#             */
-/*   Updated: 2024/09/20 17:10:02 by akloster         ###   ########.fr       */
+/*   Updated: 2024/09/22 18:26:55 by akloster         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int	update_env(char ***env, char *new_pwd)
+char	*ft_getenv(char **env, char *var)
+{
+	int	i;
+	int	j;
+
+	i = -1;
+	j = -1;
+	while (env[++i] && ft_strncmp(env[i], var, (ft_strlen(var) + 1)) != '=')
+		;
+	if (env[i])
+	{
+		while (env[i][++j] != '=')
+			;
+		return (&env[i][++j]);
+	}
+	return (env[i]);
+}
+
+static int	update_env(char ***env)
 {
 	char	*old_pwd;
 	char	*str;
+	char	*new_pwd;
 
-	old_pwd = getenv("PWD");
-	str = ft_strjoin("OLD_PWD=", old_pwd);
+	old_pwd = ft_getenv(*env, "PWD");
+	str = ft_strjoin("OLDPWD=", old_pwd);
 	if (!str)
 		return (ft_error("malloc", NO_EXIT));
 	if (change_env_var(env, str, swap_env_var))
 		return (free_arr(&str), 1);
-	free(str);
+	free_arr(&str);
+	new_pwd = ft_calloc(PATH_MAX, sizeof(char));
+	if (!new_pwd)
+		return (ft_error("malloc", NO_EXIT));
+	new_pwd = getcwd(new_pwd, PATH_MAX);
+	if (!new_pwd)
+		return (ft_error("getcwd", NO_EXIT));
 	str = ft_strjoin("PWD=", new_pwd);
+	free_arr(&new_pwd);
 	if (!str)
-		return (ft_error("malloc", NO_EXIT));
+		return (1);
 	if (change_env_var(env, str, swap_env_var))
 		return (free_arr(&str), 1);
+	free_arr(&str);
 	return (0);
 }
 
-static int incomplete_dir(char ***env, char *str) // incomplete NOTfinished
+static int	incomplete_dir(char ***env, char *str) // incomplete NOTfinished
 {
 	char	*dir;
 
@@ -41,7 +68,7 @@ static int incomplete_dir(char ***env, char *str) // incomplete NOTfinished
 	{
 		if (chdir(dir) == -1)
 			return (ft_error("cd", NO_EXIT));
-		update_env(env, dir);
+		update_env(env);
 	}
 	dir = ft_strjoin(dir, ++str);
 	if (!dir)
@@ -53,11 +80,14 @@ int	ft_cd(char ***env, t_data *data)
 {
 	if (ft_strlen(data->word) > PATH_MAX)
 		return (ft_error("cd", NO_EXIT));
-	if (!data || data->token != STRING || !ft_strncmp(data->word, "~", 1))
+	if (!data || data->token != STRING || !ft_strncmp(data->word, "~", 2)
+		|| !ft_strncmp(data->word, "~/", 3))
 	{
 		if (incomplete_dir(env, data->word))
 			return (1);
 	}
+	if (!ft_strncmp(data->word, ".", 2))
+		return (0);
 	if (data->next && data->next->token == STRING)
 	{
 		if (write(2, "minish: cd: too many arguments\n", 31) == -1)
@@ -66,6 +96,6 @@ int	ft_cd(char ***env, t_data *data)
 	}
 	if (chdir(data->word) == -1)
 		return (ft_error("cd", NO_EXIT));
-	update_env(env, data->word);
+	update_env(env);
 	return (0);
 }
