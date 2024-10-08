719a00a8516f8a55f63f637f3c6178017f4fcd40
akloster <akloster@student.s19.be>
2024-10-08 17:35:50 +0200

    make akinere change to parsing

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: akloster <akloster@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 17:34:23 by akloster          #+#    #+#             */
-/*   Updated: 2024/10/08 17:27:28 by akloster         ###   ########.fr       */
+/*   Updated: 2024/10/08 17:28:45 by akloster         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,11 +49,12 @@ static int	interactive_mode(t_exec *exec, char **envp)
 		if (arg && *arg)
 			add_history(arg);
 		data = parsing(arg, exec->env);
-/* 		data = parsing(arg, exec->env);
+		converter(&data);
+/*  		data = parsing(arg, exec->env);
 		printf("hello\n");
 		converter(&data);
 		for (t_data *temp = data; temp; temp = temp->next)
-			ft_printf("word->%s token->%d\n", temp->word, temp->token);
+			ft_printf("word->%s token->%d\n", temp->word, temp->token); */
 		my_free(&arg);
 		initializer(exec, &data);
 	}
--- src/parsing/pars_utils.c
+++ src/parsing/pars_utils.c
@@ -6,7 +6,7 @@
 /*   By: akloster <akloster@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/18 12:36:20 by akiener           #+#    #+#             */
-/*   Updated: 2024/10/06 15:01:53 by akloster         ###   ########.fr       */
+/*   Updated: 2024/10/08 17:31:04 by akloster         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,6 +64,7 @@ int	add_redir_or_pipe(t_data **data, char *arg, int *i)
 			return (-1);
 		if (addback_stack(data, str) == -1)
 			return (free(str), -1);
+		(*i)++;
 		return (free(str), 0);
 	}
 	str = malloc(sizeof (char) * 2);
